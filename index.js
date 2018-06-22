933b3a7f04561076a40e06812488f1226c471f9d
Nicolas Meseth <n.meseth@hs-osnabrueck.de>
2018-06-22 11:43:54 +0000

    First version

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import firebase from 'firebase';
+if (!firebase) {
+    console.error("Please load firebase API!");
+}
 
 firebase.initializeApp({
-    projectId: "",
-    apiKey: "",
-    databaseURL: "",
-    authDomain: "",
+    projectId: "web-engineering-project",
+    apiKey: "AIzaSyBMjgUu-f8i_HB5R6ZzNoUVcUpK57K0UHY",
+    databaseURL: "https://web-engineering-project.firebaseio.com",
+    authDomain: "web-engineering-project.firebaseapp.com",
 });
 
 const firestore = firebase.firestore();
+const auth = firebase.auth();
+
 const settings = {
     timestampsInSnapshots: true,
 };
-firestore.settings(settings);
 
-const auth = firebase.auth();
+firestore.settings(settings);
 
 export default firebase;
 
 export {
     firestore,
     auth
-};
+}
